c4f1ea7147d2d51c60caf704c6ab8c045705155d
DEEPAKGOYAL2004 <deepakgoyal201204@gmail.com>
2025-10-02 17:15:02 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";   //treat all js code as new version 
+
+console.log(3+2);
+console.log("Kashish Goyal");
+let age=21
+let name="KAshish"
+
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+const accountId=14443
+let accountEmail="deepakgoyal@gmail.com"
+var accPassword="Goyal"
+
+console.log(accountId)

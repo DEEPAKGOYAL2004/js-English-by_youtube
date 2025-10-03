094847f4783c07b3db921f026ae8076168f186f8
DEEPAKGOYAL2004 <deepakgoyal201204@gmail.com>
2025-10-03 08:09:48 +0000

    add string file

---
+++ 01_basics/strings.js
@@ -0,0 +1,32 @@
+const name="Kashish"
+const me="Deepak"
+
+// console.log(`My name is ${me} and my lovely friend name is ${name}`);
+
+const school=new String('jcs');
+
+const score=400
+// console.log(score);
+
+const mymarks=new Number(200)
+// console.log(mymarks);
+// console.log(mymarks.toString().length);
+
+// console.log(mymarks.toFixed(3));
+
+const on=265.33
+// console.log(on.toPrecision(2))
+
+// const hund=10000000
+// console.log(hund.toLocaleString('en-IN'));
+console.log(Math)
+console.log(Math.abs(-25));
+
+console.log(Math.random());
+//if i want a number between a certain range of numbers 
+
+const min=10;
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
